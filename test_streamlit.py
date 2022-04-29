5d43d71f359d2baa6e41dcf0842ea47146d6c9ab
Antoine Barré <abarre@mathworks.com>
2022-04-29 14:34:19 +0200

    feat: essai bourse avec streamlit

--- test_streamlit.py
+++ test_streamlit.py
@@ -5,9 +5,12 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
+import grisbi
+
+grisbi.get_data_from_Yahoo("GE")
+df = grisbi.load_data_from_csv("GE")
+
+
 
 st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+st.write(df)
